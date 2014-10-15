7c8a43b5c6c22943811a14ad83c05bdeb62b7d48
Todd Anderson <bustardcelly@gmail.com>
2014-10-14 21:17:31 -0400

    flexhing out exchange form.

--- client/script/view/exchange-form.js
+++ client/script/view/exchange-form.js
@@ -3,10 +3,59 @@
 var React = require('react');
 
 var Form = React.createClass({displayName: 'ExchangeForm', 
+  handleSubmit: function(event) {
+    event.preventDefault();
+    console.log('submit new exchange.');
+    return false;
+  },
   render: function() {
     return (
-      React.DOM.div(null,
-        React.DOM.p(null, 'hello,world')
+      React.DOM.div({
+        id: 'exchange-form',
+        className: 'form-inline',
+        role: 'form',
+        action: '#'
+      },
+        React.DOM.div({
+          className: 'form-group'
+        },
+          React.DOM.h3(null, 'New Exchange'),
+          React.DOM.input({
+            id: "exchange-title-input",
+            className: 'form-control input-md',
+            placeholder: 'Title',
+            type: 'text'
+          })
+        ),
+        React.DOM.div({
+          className: 'form-group'
+        },
+          React.DOM.input({
+            id: "exchange-source-input",
+            className: 'form-control input-md',
+            placeholder: 'Source',
+            type: 'text'
+          })
+        ),
+        React.DOM.div({
+          className: 'form-group'
+        },
+          React.DOM.textarea({
+            id: "exchange-description-input",
+            className: 'form-control input-md',
+            placeholder: 'Description'
+          })
+        ),
+        React.DOM.div({
+          className: 'form-group'
+        },
+          React.DOM.button({
+            id: 'exchange-submit-button',
+            type: 'submit',
+            className: 'btn btn-success btn-md',
+            onClick: this.handleSubmit
+          }, 'submit')
+        )
       )
     );
   }
