36c22097ee92ec22ddc273b4d450a8223c71523c
Todd Anderson <bustardcelly@gmail.com>
2017-04-26 09:04:56 -0400

    better S3 deployment scripts.

--- aws/last_seq.sh
+++ aws/last_seq.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo "welcome"
 
-CREDS=../.creds
-FILE=../.version
-LOG=./gift-giving-analysis-backup.log
+CWD=$(pwd)
+CREDS="${CWD}/.creds"
+FILE="${CWD}/.version"
+LOG="${CWD}/aws/gift-giving-analysis-backup.log"
 DATE=$(date)
 
 while [[ $# -gt 1 ]]
@@ -20,25 +22,50 @@ do
   esac
 done
 
-version=`cat $FILE`
-creds=`cat $CREDS`
-IFS=', ' read -r -a array <<< $creds
+echo "------" >> "$LOG"
+echo "$DATE" >> "$LOG"
+echo "------" >> "$LOG"
+
+version=$(cat "$FILE")
+creds=$(cat "$CREDS")
+IFS=', ' read -r -a array <<< "$creds"
 username=${array[0]}
 password=${array[1]}
 endpoint="http://${username}:${password}@127.0.0.1:5984/exchange/_changes"
 
-echo "Checking version in ${FILE}.: ${version}" >> $LOG
+echo "Username:${username}, Password:${password}"
+echo "Will ping ${endpoint}..."
 
-payload=`curl -s -X GET -H "Content-Type: application/json" $endpoint | \
+echo "Checking version in ${FILE}.: ${version}" >> "$LOG"
+
+payload=$(curl -sv -X GET -H "Content-Type: application/json" "${endpoint}" | \
         grep -m1 -e '.*\"last_seq":\(.*\).*' | \
         awk -F ":" '{print $2}' | \
-        awk -F "}" '{print $1}'`
+        awk -F "}" '{print $1}')
 
+# If empty string, most likely something went wrong in requesting info.
+if [ -z "$payload" ]; then
+  echo "Something went wrong in accessing changes."
+  echo "Something went wrong in accessing changes." >> "$LOG"
+  exit 1
+fi
+
+# Else the version is different, so let's back it up.
 if [ "$version" != "$payload" ]; then
-  echo "Previous version: ~ $version ~ doesn't match current: ~ $payload ~."
-  echo `node aws/upload-remote.js`
-  echo $payload > $FILE
-  echo "Nightly Backup Successful: $DATE" >> $LOG
+  echo "Previous version: ~ $version ~ doesn't match current: ~ $payload ~." >> "$LOG"
+  if [ ! -d "${CWD}/aws/couchdb-dump" ]; then
+    git clone https://github.com/danielebailo/couchdb-dump.git "${CWD}/aws/couchdb-dump"
+  fi
+  DUMP_DIR="${CWD}/aws/dump"
+  DUMP_BASH="${CWD}/aws/couchdb-dump/couchdb-backup.sd"
+  mkdir -p DUMP_DIR
+  bash "$DUMP_BASH" -b -H 127.0.0.1 -d exchange -f "${DUMP_DIR}/exchange.json" -u "${username}" -p "${password}"
+  bash "$DUMP_BASH" -b -H 127.0.0.1 -d gift -f "${DUMP_DIR}/gift.json" -u "${username}" -p "${password}"
+  bash "$DUMP_BASH" -b -H 127.0.0.1 -d motif -f "${DUMP_DIR}/motif.json" -u  "${username}" -p "${password}"
+  bash "$DUMP_BASH" -b -H 127.0.0.1 -d reproduction -f "${DUMP_DIR}/reproduction.json" -u  "${username}" -p "${password}"
+  node "${CWD}/aws/upload-remote.js"
+  echo "$payload" > "$FILE"
+  echo "Nightly Backup Successful: $DATE" >> "$LOG"
 else
-  echo "Already up to date. $DATE" >> $LOG
+  echo "Already up to date. $DATE" >> "$LOG"
 fi
--- aws/run.sh
+++ aws/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+backup-couchdb-to-s3 --dbUrl http://localhost:5984 --dbName exchange --s3BucketName gift-giving-analysis --awsCredentialsPath ./credentials
 backup-couchdb-to-s3 --dbUrl http://localhost:5984 --dbName gift --s3BucketName gift-giving-analysis --awsCredentialsPath ./credentials
 backup-couchdb-to-s3 --dbUrl http://localhost:5984 --dbName motif --s3BucketName gift-giving-analysis --awsCredentialsPath ./credentials
 backup-couchdb-to-s3 --dbUrl http://localhost:5984 --dbName reproduction --s3BucketName gift-giving-analysis --awsCredentialsPath ./credentials
